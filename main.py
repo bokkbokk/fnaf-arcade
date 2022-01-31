462b0f967965eaaf1b4f7042685de8e2e018d174
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2022-01-31 11:54:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ my_sprite = sprites.create(assets.image("""
 """), SpriteKind.player)
 controller.move_sprite(None)
 controller.move_sprite(my_sprite)
-characterAnimations.loop_frames(my_Sprite, [], 500, 0)
