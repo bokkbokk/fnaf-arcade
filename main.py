c4f4fa6c06531cffce8df0f813604844a39e468e
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2022-01-31 12:26:34 -0700

    Update main.blocks, main.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 my_sprite = sprites.create(assets.image("""
     myImage
 """), SpriteKind.player)
-controller.move_sprite(None)
 controller.move_sprite(my_sprite)
+
+def on_forever():
+    pass
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let my_sprite = sprites.create(assets.image`
     myImage
 `, SpriteKind.Player)
-controller.moveSprite(null)
 controller.moveSprite(my_sprite)
+forever(function on_forever() {
+    
+})
